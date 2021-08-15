69db4f10b8ae9b1bdd6d717fb184daf4413fdff3
huthaifaTayeh <huthaifazmi@gmail.com>
2021-08-15 06:59:15 +0300

    Added JavaScript folder to the Repo

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World!")
+let name = "Hello"
+console.log(name)
+let player = {
+    name: 'Huthaifa',
+    age: 25,
+    eyesColor: 'Brown',
+    height: 1.83,
+    weight: 92
+}
+console.log(player)
+player.weight = 91.5
+console.log(player)
