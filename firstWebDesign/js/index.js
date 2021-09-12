2a3c9bc2487900c4e3e857204ca5f9395b72b9b5
Huthaifa Taya <69609607+huthaifaTayeh@users.noreply.github.com>
2021-09-12 21:23:51 +0300

    dynamic render for public groups
    Made the render of public groups dynamic using JS

--- firstWebDesign/js/index.js
+++ firstWebDesign/js/index.js
@@ -1,9 +1,31 @@
-/*
-    Profiles rendering
-*/
+let path = window.location.pathname;
+let currentPage = path.split("/").pop();
+console.log(currentPage);
+// /*
+//     Profiles rendering
+// */
 
-let userName = "حذيفة تايه";
-let imgURL = "/FaqraouWeb/images/user.png";
-var imageContainer = document.getElementById("pro-img-con");
-imageContainer.querySelector("img").src = imgURL;
-imageContainer.querySelector("h1").innerHTML = userName;
+if (currentPage === "profile.html") {
+  let userName = "حذيفة تايه";
+  let imgURL = "/FaqraouWeb/images/user.png";
+  var imageContainer = document.getElementById("pro-img-con");
+  imageContainer.querySelector("img").src = imgURL;
+  imageContainer.querySelector("img").alt = `${userName}'s imge `;
+  imageContainer.querySelector("h1").innerHTML = userName;
+}
+/* 
+    Render Public Groups
+*/
+if (currentPage === "groups.html") {
+  let groupName = " اسم المجموعة";
+  let groupDescription =
+    "لوريم ايبسوم هو نموذج فتراضي يوضع في التصاميم لتعرض على العميل ليتصور";
+  let groupImgSrc = "/FaqraouWeb/images/group_icon.png";
+  let pubGroupsViews = document.getElementsByClassName("group-view-block");
+  for (let group = 0; group < pubGroupsViews.length; group++) {
+    pubGroupsViews[group].querySelector("img").src = groupImgSrc;
+    pubGroupsViews[group].querySelector("img").alt = `${groupName} icon`;
+    pubGroupsViews[group].querySelector("h5").innerHTML = groupName;
+    pubGroupsViews[group].querySelector("p").innerHTML = groupDescription;
+  }
+}
